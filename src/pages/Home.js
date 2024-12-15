b7bdec58f0c603f22e4c47abe420f7dc23a27c26
Dennis <dennis.badagliacca444@gmail.com>
2024-12-16 00:46:53 +0100

    Feat: update version to 0.8.0 and sort orders by urgency level on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,26 @@ import useSession from "../hooks/useSession";
 import NoOrders from "../components/Orders/NoOrders";
 import useRealtime from "../hooks/useRealTime";
 
+const urgencyLevels = {
+  Bassa: 0,
+  Media: 1,
+  Alta: 2,
+  Urgente: 3,
+};
+
 const Home = () => {
   let orders = useRealtime("orders");
+
+  const sortedOrders = orders.sort((a, b) => {
+    return urgencyLevels[b.urgency] - urgencyLevels[a.urgency];
+  });
 
   return (
     <div>
       {orders.length === 0 ? (
         <NoOrders />
       ) : (
-        orders.map((order) => <MainTable key={order.id} order={order} />)
+        sortedOrders.map((order) => <MainTable key={order.id} order={order} />)
       )}
     </div>
   );
